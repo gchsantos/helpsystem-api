12d54f90f4b6987ac4a578769a57233e94d45429
gchsantos <gchsantos@gmail.com>
2023-06-04 23:56:19 -0300

    feat(sales): create sales contants

---
+++ sales/constants.py
@@ -0,0 +1,17 @@
+
+class ErrorMessages:
+    MISSING_VALUE_MESSAGE = 'Preencha o campos do formulário: '
+    CUSTOMER_DOES_NOT_EXIST = 'Cliente inexistente.'
+    SELLER_DOES_NOT_EXIST = 'Vendedor inexistente.'
+    PLAN_DESC_DOES_NOT_EXIST = 'Plano inexistente.'
+    PLAN_ALREADY_EXIST = 'Já existe um plano ativo para esse cliente.'
+    CUSTOMER_PLAN_DOES_NOT_EXIST = 'Este plano de cliente não existe.'
+    NOT_PERMITTED_OPERATION = 'Operação não permitida.'
+
+
+class SuccessMessages:
+    SELL_CREATED = 'Venda realizada com sucesso.'
+    PLAN_DESACTIVATED = 'Plano desativado com sucesso.'
+
+
+PERMITTED_OPERATIONS = ['deactivate']

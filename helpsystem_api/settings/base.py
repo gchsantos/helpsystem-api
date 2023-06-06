14fd49a8313376ad4b22bafbbf031f1bae38ccbd
gchsantos <gchsantos@gmail.com>
2023-06-06 07:12:39 -0300

    chore(cd): add gunicorn to requirements

--- helpsystem_api/settings/base.py
+++ helpsystem_api/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'helpsystem_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
@@ -112,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -124,5 +126,5 @@ PIX_DESCRIPTION = 'HelpSystem Plano '
 PIX_MERCHANT_NAME = 'HELPSYSTEMLTDA'
 PIX_MERCHANT_CITY = 'Bebedouro - SP'
 
-QRCODES_URL = '/media/qrcodes/'
+QRCODES_URL = '/media/qrcodes'
 QRCODES_ROOT = BASE_DIR / 'media/qrcodes'
--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
 NAME="helpsystem_api"
-DJANGO_WSGI_MODULE=helpsystem_api.wsgi
-NUM_WORKERS=3
 
 if ! python manage.py test --k; then
     echo '[ ALERT: Application not pass in the tests. ]'
@@ -11,13 +9,4 @@ fi
 
 python manage.py makemigrations
 python manage.py migrate
-
-gunicorn ${DJANGO_WSGI_MODULE}:application \
-    --name ${NAME} \
-    --timeout 120 \
-    --workers ${NUM_WORKERS} \
-    --threads 2 \
-    --bind 0.0.0.0:8000 \
-    --log-config gunicorn.conf \
-    --log-syslog-prefix gunicorn \
-    --log-level=info
+python manage.py runserver 0.0.0.0:8000

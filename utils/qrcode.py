d48a0d9d0c7decd014d81c799a89bfdf6f3b3e99
gchsantos <gchsantos@gmail.com>
2023-06-06 21:04:29 -0300

    fix(qrcode): adjust qr path and root for generation function

--- utils/qrcode.py
+++ utils/qrcode.py
@@ -6,7 +6,7 @@ import time
 def generate_qr(data, name=str(time.time())):
     img = make(data)
     img_name = f"qr_{name}.png"
-    qr_path = f"{settings.QRCODES_ROOT}\{img_name}"
-    qr_url = f"{settings.QRCODES_URL}\{img_name}"
+    qr_path = f"{settings.QRCODES_ROOT}/{img_name}"
+    qr_url = f"{settings.QRCODES_URL}/{img_name}"
     img.save(qr_path)
     return qr_url
